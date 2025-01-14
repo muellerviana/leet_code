d822b0ba20999eb82b955a0e80e6dbb05640492d
muellerviana <muellerviana@gmail.com>
2025-01-13 23:52:40 -0300

    roman to integer

--- roman_to_integer.rb
+++ roman_to_integer.rb
@@ -1,7 +1,7 @@
 require 'byebug'
 
-def roman_to_int(s) 
-  conversion = {
+def roman_to_integer(s)
+  roman_to_int = {
     'I' => 1,
     'V' => 5,
     'X' => 10,
@@ -10,12 +10,32 @@ def roman_to_int(s)
     'D' => 500,
     'M' => 1000
   }
+  
+  result = 0
+  i = 0
+  
 
-  s.each_char do |char|
-    puts char
+
+
+  while i < s.length - 1
+    byebug
+    current_value = roman_to_int[s[i]] # a - 1 | 
+    next_value = roman_to_int[s[i + 1]] # a = 1 |
+    
+    if current_value < next_value # a - 1
+      result -= current_value
+    else
+      result += current_value
+    end
+    
+    i += 1
   end
+  
+  result += roman_to_int[s[-1]]
+  
+  result
 end
 
-puts roman_to_int('III')
-puts roman_to_int('LVIII')
-puts roman_to_int('MCMXCIV')
+puts roman_to_integer('III') #3
+puts roman_to_integer('LVIII') #5
+puts roman_to_integer('MCMXCIV') #7
